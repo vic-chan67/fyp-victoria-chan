f31fb24ecae5f5d54f620dab3811ae501a0921f7
Victoria Chan <victoriachan67@gmail.com>
2025-02-17 23:45:06 +0000

    Model building function

---
+++ model.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
+
+def build_model():
+
+    model = Sequential([
+        Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+        MaxPooling2D(pool_size=(2, 2)),
+
+        Conv2D(64, (3, 3), activation='relu'),
+        MaxPooling2D(pool_size=(2, 2)),
+
+        Flatten(),
+
+        Dense(128, activation='relu'),
+        Dropout(0.5),
+
+        Dense(43, activation='softmax')
+    ])
+
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer='adam',
+        metrics=['accuracy']
+    )
+
+    return model
