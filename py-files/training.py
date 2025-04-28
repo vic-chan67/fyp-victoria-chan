a1dc54dc7c1d7a3715dab3b2eda8c1ef7616d8c2
Victoria Chan <victoriachan67@gmail.com>
2025-04-28 08:55:35 +0100

    Accuracy and loss visualisations added

--- py-files/training.py
+++ py-files/training.py
@@ -5,6 +5,7 @@ import load_data
 import model
 import tensorflow as tf
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping # type: ignore
+import matplotlib.pyplot as plt
 
 # Load dataset
 print("---------- Loading dataset ----------")
@@ -32,3 +33,29 @@ history = cnn_model.fit(
 # Save model
 cnn_model.save("models/final_model.keras")
 print("Model trained and saved")
+
+# Plot training & validation accuracy values
+plt.figure(figsize=(12, 5))
+
+# Accuracy
+plt.subplot(1, 2, 1)
+plt.plot(history.history['accuracy'], label='Train Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.title('Model Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.grid(True)
+
+# Loss
+plt.subplot(1, 2, 2)
+plt.plot(history.history['loss'], label='Train Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.title('Model Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+plt.grid(True)
+
+plt.tight_layout()
+plt.show()
