18b6fe0d5efb2667a3bf9d8ee9122235227e7865
Victoria Chan <victoriachan67@gmail.com>
2025-04-29 22:52:33 +0100

    Updated to return image and bounding boxes instead of saving image

--- py-files/detection.py
+++ py-files/detection.py
@@ -3,16 +3,17 @@
 
 from ultralytics import YOLO
 import cv2
-import os
+# import os
 from descriptions import DESCRIPTIONS
 
 # Load YOLOv8 model
-model = YOLO("runs/detect/train/weights/best.pt")
+model = YOLO("py-files/runs/detect/train4/weights/best.pt")
 
 def detect_signs(image_path):  #take image path as input
     image = cv2.imread(image_path)
-    results = model(image_path)  #pass image to YOLOv8 for prediction
+    results = model(image)  #pass image to YOLOv8 for prediction
 
+    bounding_boxes = []
     for r in results:  #loop through results (r is a result object)
         boxes = r.boxes
         for box in boxes:
@@ -20,17 +21,20 @@ def detect_signs(image_path):  #take image path as input
             class_id = int(box.cls[0])  #get class id (sign type)
             #confidence = float(box.conf[0])  #get confidence score
             label = DESCRIPTIONS.get(class_id, f"Class {class_id}")  #get label from dictionary
+            bounding_boxes.append((x1, y1, x2, y2, class_id, label))
 
             # Draw bounding box and label
-            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.putText(image, label, (x1, y1 - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            # cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            # cv2.putText(image, label, (x1, y1 - 10),
+            #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    return image, bounding_boxes
 
-    # Save output image
-    out_path = image_path.replace("gtsdb-data", "detect-data").replace(".ppm", "_yolo.jpg")
-    os.makedirs(os.path.dirname(out_path), exist_ok=True)
-    cv2.imwrite(out_path, image)
-    print(f"Output image saved")
+    # # Save output image
+    # out_path = image_path.replace("gtsdb-data", "detect-data").replace(".ppm", "_yolo.jpg")
+    # os.makedirs(os.path.dirname(out_path), exist_ok=True)
+    # cv2.imwrite(out_path, image)
+    # print(f"Output image saved")
 
 if __name__ == "__main__":
-    detect_signs("gtsdb-data/00000.ppm")
+    image, bounding_boxes = detect_signs("datasets/gtsdb-yolo/images/val/00090.jpg")
+    print(f"Detected {len(bounding_boxes)} signs")
