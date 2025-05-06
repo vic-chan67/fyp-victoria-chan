e995461012aa41aab8ac41f91c313384f6ea2628
Victoria Chan <victoriachan67@gmail.com>
2025-05-07 00:24:47 +0100

    Added time counter

--- py-files/full_pipeline.py
+++ py-files/full_pipeline.py
@@ -4,6 +4,8 @@
 import tensorflow as tf
 import numpy as np
 import cv2
+import time
+
 from detection import detect_signs
 from cropping import crop_signs
 from descriptions import get_description
@@ -19,13 +21,16 @@ def preprocess(img):
 #---
 
 # User language selection
-lang = input("Enter target language code (fr, es, de)/(full list in \"lang_codes.txt\"): ").strip().lower()
+#lang = input("Enter target language code (fr, es, de)/(full list in \"lang_codes.txt\"): ").strip().lower()
+lang = "fr"
 
 # Load model
 cnn_model = tf.keras.models.load_model("py-files/models/final_model.keras")
 
+start_time = time.time()
+
 # Image path
-image_path = "datasets/gtsdb-yolo/images/val/00090.jpg"
+image_path = "datasets/gtsdb-yolo/images/val/00788.jpg"
 
 # Detection
 image, bboxes = detect_signs(image_path)
@@ -43,3 +48,7 @@ for idx, sign in enumerate(cropped_signs):
     label = get_description(predicted_class)
     translation = translate_text(label, lang)
     print(f"Sign {idx + 1}: predicted class {predicted_class} = {label}, translated = {translation}")
+
+end_time = time.time()
+
+print(f"\nTotal time taken: {end_time - start_time:.2f} seconds")
