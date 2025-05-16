709cb98e2cf693a339bbccc2a399f58b5c71d8a4
Victoria Chan <victoriachan67@gmail.com>
2025-05-17 00:03:20 +0100

    Working full pipeline with React Expo

--- py-files/main.py
+++ py-files/main.py
@@ -2,53 +2,46 @@
 # Main script for running the Flask server
 # Must be ran before running React app
 
-from flask import Flask, jsonify, request
 import numpy as np
-from PIL import Image
 import base64
-from io import BytesIO
+import tempfile
+import os
 import descriptions
 import tensorflow as tf
+from flask import Flask, jsonify, request
+from PIL import Image
+from io import BytesIO
 from flask_cors import CORS
+from full_pipeline import run_pipeline
 
 app = Flask(__name__)
 CORS(app)
 
-cnn_model = tf.keras.models.load_model("models/final_cnn.keras")
+# Load model
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CNN_MODEL_PATH = os.path.join(BASE_DIR, "models", "final_cnn.keras")
 
-# Predict route - accepts POST requests with image and predicts the class
-@app.route("/predict", methods=["POST"])
-def predict():
-    # Get image from request
-    image_file = request.files['image']
+cnn_model = tf.keras.models.load_model(CNN_MODEL_PATH)
 
-    # Open image
-    img = Image.open(image_file)
-    img = img.convert('RGB')
+# Pipeline route - accepts POST requests with image and runs the full pipeline
+@app.route("/pipeline", methods=["POST"])
+def pipeline():
+    try:
+        if 'image' not in request.files:
+            return jsonify({"error": "No image provided"}), 400
+        
+        image_file = request.files['image']  #get image from request
+        lang = request.args.get('lang', 'en')  #English default
 
-    # Normalisation
-    img = img.resize((32, 32))
-    img_array = np.array(img) / 255.0
-    img_array = np.expand_dims(img_array, axis=0)  #add batch dimension
+        temp_path = os.path.join("temp_image.jpg")  #temporary path for saving image
+        image_file.save(temp_path)
 
-    # Prediction
-    prediction = cnn_model.predict(img_array)
-    predicted_label = np.argmax(prediction)
-
-    predicted_label = int(predicted_label)  #convert to int
-    description = descriptions.get_description(predicted_label)  #get description
-
-    # Convert image to base64 for sending as JSON
-    buffered = BytesIO()
-    img.save(buffered, format="JPEG")
-    img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
-
-    # Send prediction result and image in base64
-    return jsonify({
-        "image": img_str,
-        "predicted_label": predicted_label,
-        "description": description
-    })
+        results = run_pipeline(temp_path, lang)  #run pipeline
+        return jsonify({"results": results}), 200  # return results
+    
+    except Exception as e:  #catch any exceptions
+        print(f"Error in pipeline: {e}")
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)  #port 5001, avoids conflict with LibreTranslate
